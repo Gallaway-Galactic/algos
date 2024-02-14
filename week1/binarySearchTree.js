c6f4aa8764ebc2f307efcccd0a62e18bfb0cafbb
Joshua <gallaway.joshua@gmail.com>
2024-02-13 20:22:36 -0700

    morning algo practice

---
+++ week1/binarySearchTree.js
@@ -0,0 +1,289 @@
+/**
+ * Class to represent a Node in a Binary Search Tree (BST).
+ */
+class BSTNode {
+  /**
+   * Constructs a new instance of a BST node.
+   * @param {number} data The integer to store in the node.
+   */
+  constructor(data) {
+    this.data = data;
+    /**
+     * These properties are how this node is connected to other nodes to form
+     * the tree. Similar to .next in a SinglyLinkedList except a BST node can
+     * be connected to two other nodes. To start, new nodes will not be
+     * connected to any other nodes, these properties will be set after
+     * the new node is instantiated.
+     *
+     * @type {BSTNode|null}
+     */
+    this.left = null;
+    /** @type {BSTNode|null} */
+    this.right = null;
+  }
+}  //! END BSTNode
+
+/**
+ * Represents an ordered tree of nodes where the data of left nodes are <= to
+ * their parent and the data of nodes to the right are > their parent's data.
+ */
+class BinarySearchTree {
+
+  constructor() {
+    /**
+     * Just like the head of a linked list, this is the start of our tree which
+     * branches downward from here.
+     *
+     * @type {BSTNode|null}
+     */
+    this.root = null;
+  }
+
+  /**
+   * Determines if this tree is empty.
+   * - Time: O(?).
+   * - Space: O(?).
+   * @returns {boolean} Indicates if this tree is empty.
+   */
+  isEmpty() {
+    return this.root === null
+    // if(this.root === null){
+    //   return true
+    // } else {
+    //   return false
+    // }
+  }
+
+  /**
+   * Retrieves the smallest integer data from this tree.
+   * - Time: O(?).
+   * - Space: O(?).
+   * @param {Node} current The node that is currently accessed from the tree as
+   *    the tree is being traversed.
+   * @returns {number} The smallest integer from this tree.
+   */
+  min(current = this.root) {
+
+    while (current.left) {
+      current = current.left
+    }
+
+    return current.data
+  }
+
+  /**
+   * Retrieves the smallest integer data from this tree.
+   * - Time: O(?).
+   * - Space: O(?).
+   * @param {Node} current The node that is currently accessed from the tree as
+   *    the tree is being traversed.
+   * @returns {number} The smallest integer from this tree.
+  */
+  minRecursive(current = this.root) { }
+
+  /**
+   * Retrieves the largest integer data from this tree.
+   * - Time: O(?).
+   * - Space: O(?).
+   * @param {Node} current The node that is currently accessed from the tree as
+   *    the tree is being traversed.
+   * @returns {number} The largest integer from this tree.
+  */
+  max(current = this.root) {
+
+    while (current.right) {
+      current = current.right
+    }
+    return current.data
+  } //! MONDAY END
+
+  /**
+ * Determines if this tree contains the given searchVal.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {number} searchVal The number to search for in the node's data.
+ * @returns {boolean} Indicates if the searchVal was found.
+ */
+  contains(searchVal) {
+    if (this.isEmpty()) {
+      return false;
+    }
+    let current = this.root;
+    while (current) {
+      if (current.data == searchVal) {
+        return true;
+      }
+      if (current.data > searchVal) {
+        current = current.left
+      }
+      else if (current.data < searchVal) {
+        current = current.right
+      }
+    }
+    return false;
+  }
+
+  /**
+   * Determines if this tree contains the given searchVal.
+   * - Time: O(?).
+   * - Space: O(?).
+   * @param {number} searchVal The number to search for in the node's data.
+   * @returns {boolean} Indicates if the searchVal was found.
+   */
+  containsRecursive(searchVal, current = this.root) { 
+      if(current === null){
+        return false;
+      }
+      if (current.data === searchVal) {
+        return true;
+      }
+      if (current.data > searchVal) {
+        return this.containsRecursive(searchVal, current.left)
+      }
+      return this.containsRecursive(searchVal, current.right)
+  }
+  /**
+   * Calculates the range (max - min) from the given startNode.
+   * - Time: O(?).
+   * - Space: O(?).
+   * @param {Node} startNode The node to start from to calculate the range.
+   * @returns {number|null} The range of this tree or a sub tree depending on if the
+   *    startNode is the root or not.
+   */
+  range(startNode = this.root) {
+    if (!startNode) {
+      return null;
+    }
+    let minVal = this.min(startNode)
+    let maxVal = this.max(startNode)
+    /*
+    also can do:
+    return this.max(startNode) - this.min(startNode);
+    */
+    return maxVal - minVal;
+  }
+
+  /**
+     * Retrieves the largest integer data from this tree.
+     * - Time: O(?).
+     * - Space: O(?).
+     * @param {Node} current The node that is currently accessed from the tree as
+     *    the tree is being traversed.
+     * @returns {number} The largest integer from this tree.
+     */
+  maxRecursive(current = this.root) { }
+
+  // Logs this tree horizontally with the root on the left.
+  print(node = this.root, spaceCnt = 0, spaceIncr = 10) {
+    if (!node) {
+      return;
+    }
+
+    spaceCnt += spaceIncr;
+    this.print(node.right, spaceCnt);
+
+    console.log(
+      " ".repeat(spaceCnt < spaceIncr ? 0 : spaceCnt - spaceIncr) +
+      `${node.data}`
+    );
+
+    this.print(node.left, spaceCnt);
+  }
+
+
+} //! END BinarySearchTree
+
+
+
+
+
+
+
+
+const emptyTree = new BinarySearchTree();
+const oneNodeTree = new BinarySearchTree();
+oneNodeTree.root = new BSTNode(10);
+
+/* twoLevelTree
+        root
+        10
+      /   \
+    5     15
+*/
+const twoLevelTree = new BinarySearchTree();
+twoLevelTree.root = new BSTNode(10);
+twoLevelTree.root.left = new BSTNode(5);
+twoLevelTree.root.right = new BSTNode(15);
+
+/* threeLevelTree 
+        root
+        10
+      /   \
+    5     15
+  / \    / \
+2   6  13  
+*/
+const threeLevelTree = new BinarySearchTree();
+threeLevelTree.root = new BSTNode(10);
+threeLevelTree.root.left = new BSTNode(5);
+threeLevelTree.root.left.left = new BSTNode(2);
+threeLevelTree.root.left.right = new BSTNode(6);
+threeLevelTree.root.right = new BSTNode(15);
+threeLevelTree.root.right.left = new BSTNode(13);
+
+/* fullTree
+                    root
+                <-- 25 -->
+              /            \
+            15             50
+          /    \         /    \
+        10     22      35     70
+      /   \   /  \    /  \   /  \
+    4    12  18  24  31  44 66  90
+*/
+/***************** Uncomment after insert method is created. ****************/
+// const fullTree = new BinarySearchTree();
+// fullTree
+//   .insert(25)
+//   .insert(15)
+//   .insert(10)
+//   .insert(22)
+//   .insert(4)
+//   .insert(12)
+//   .insert(18)
+//   .insert(24)
+//   .insert(50)
+//   .insert(35)
+//   .insert(70)
+//   .insert(31)
+//   .insert(44)
+//   .insert(66)
+//   .insert(90);
+
+const myBST = new BinarySearchTree()
+
+const node1 = new BSTNode(50)
+const node2 = new BSTNode(40)
+const node3 = new BSTNode(30)
+const node4 = new BSTNode(20)
+const node5 = new BSTNode(60)
+const node6 = new BSTNode(45)
+const node7 = new BSTNode(55)
+const node8 = new BSTNode(75)
+const node9 = new BSTNode(95)
+
+
+myBST.root = node1
+myBST.root.left = node2
+myBST.root.left.left = node3
+myBST.root.left.left.left = node4
+myBST.root.right = node5
+node2.right = node6
+node5.left = node7
+node5.right = node8
+node8.right = node9
+
+
+
+
+
---
+++ week1/singlelyLinkedList.js
@@ -0,0 +1,348 @@
+/**
+ * A class to represents a single item of a SinglyLinkedList that can be
+ * linked to other Node instances to form a list of linked nodes.
+ */
+class ListNode {
+    /**
+     * Constructs a new Node instance. Executed when the 'new' keyword is used.
+     * @param {any} data The data to be added into this new instance of a Node.
+     *    The data can be anything, just like an array can contain strings,
+     *    numbers, objects, etc.
+     * @returns {ListNode} A new Node instance is returned automatically without
+     *    having to be explicitly written (implicit return).
+     */
+    constructor(data) {
+        this.data = data;
+        /**
+         * This property is used to link this node to whichever node is next
+         * in the list. By default, this new node is not linked to any other
+         * nodes, so the setting / updating of this property will happen sometime
+         * after this node is created.
+         *
+         * @type {ListNode|null}
+         */
+        this.next = null;
+    }
+}
+
+/**
+ * This class keeps track of the start (head) of the list and to store all the
+ * functionality (methods) that each list should have.
+ */
+class SinglyLinkedList {
+    /**
+     * Constructs a new instance of an empty linked list that inherits all the
+     * methods.
+     * @returns {SinglyLinkedList} The new list that is instantiated is implicitly
+     *    returned without having to explicitly write "return".
+     */
+    constructor() {
+        /** @type {ListNode|null} */
+        this.head = null;
+    }
+
+    /**
+     * Determines if this list is empty.
+     * - Time: O(?).
+     * - Space: O(?).
+     * @returns {boolean}
+     */
+    isEmpty() {
+        return this.head === null;
+    }
+
+    /**
+     * Creates a new node with the given data and inserts it at the back of
+     * this list.
+     * - Time: O(?).
+     * - Space: O(?).
+     * @param {any} data The data to be added to the new node.
+     * @returns {SinglyLinkedList} This list.
+     */
+    insertAtBack(data) {
+
+        // create a new node to add to list
+        let newNode = new ListNode(data);
+
+        // if empty
+        if (this.isEmpty()) {
+            this.head = newNode;
+            return this;
+        }
+
+
+        // if not empty
+        let runner = this.head
+        while (runner.next !== null) {
+            runner = runner.next;
+        }
+        // console.log(runner)
+
+        runner.next = newNode;
+        return this;
+
+
+    }
+
+    /**
+     * Creates a new node with the given data and inserts it at the back of
+     * this list.
+     * - Time: O(?).
+     * - Space: O(?).
+     * @param {any} data The data to be added to the new node.
+     * @param {?ListNode} runner The current node during the traversal of this list
+     *    or null when the end of the list has been reached.
+     * @returns {SinglyLinkedList} This list.
+     */
+    insertAtBackRecursive(data, runner = this.head) { }
+
+    /**
+     * Calls insertAtBack on each item of the given array.
+     * - Time: O(n * m) n = list length, m = arr.length.
+     * - Space: O(1) constant.
+     * @param {Array<any>} vals The data for each new node.
+     * @returns {SinglyLinkedList} This list.
+     */
+    insertAtBackMany(vals) {
+        for (const item of vals) {
+            this.insertAtBack(item);
+        }
+        return this;
+    }
+
+    /**
+     * Converts this list into an array containing the data of each node.
+     * - Time: O(n) linear.
+     * - Space: O(n).
+     * @returns {Array<any>} An array of each node's data.
+     */
+    toArr() {
+        const arr = [];
+        let runner = this.head;
+
+        while (runner) {
+            arr.push(runner.data);
+            runner = runner.next;
+        }
+        return arr;
+    }
+
+    /**
+     * Creates a new node with the given data and inserts that node at the front
+     * of this list.
+     * - Time: (?).
+     * - Space: (?).
+     * @param {any} data The data for the new node.
+     * @returns {SinglyLinkedList} This list.
+     */
+    insertAtFront(data) {
+        // create a new node
+        let newNode = new ListNode(data)
+
+        // set new node's next value to current head
+        newNode.next = this.head;
+        // set the head of the SLL to the new node
+        this.head = newNode;
+        // return the instance of the SLL
+        return this;
+
+    }
+
+    /**
+     * Removes the first node of this list.
+     * - Time: (?).
+     * - Space: (?).
+     * @returns {any} The data from the removed node.
+     */
+    removeHead() {
+        let oldhead = this.head;
+        this.head = this.head.next
+        oldhead.next = null
+        console.log(this)
+        return oldhead.data
+    }
+
+    /**
+* Removes the last node of this list.
+* - Time: O(?).
+* - Space: O(?).
+* @returns {any} The data from the node that was removed.
+*/
+    removeBack() {
+        if (this.isEmpty()) {
+            return null;
+        }
+        let runner = this.head;
+        if (!runner.next) {
+            return this.removeHead()
+        }
+        while (runner.next.next) {
+            runner = runner.next;
+        }
+        let removing = runner.next.data;
+        runner.next = null;
+        console.log(runner)
+        return removing;
+    }
+
+    /**
+     * Determines whether or not the given search value exists in this list.
+     * - Time: O(?).
+     * - Space: O(?).
+     * @param {any} val The data to search for in the nodes of this list.
+     * @returns {boolean}
+     */
+    contains(val) {
+        let runner = this.head;
+
+        while (runner) {
+            if (runner.data === val) {
+                return true;
+            }
+            runner = runner.next;
+        }
+        return false;
+    }
+
+    /**
+* Retrieves the data of the second to last node in this list.
+* - Time: O(?).
+* - Space: O(?).
+* @returns {any} The data of the second to last node or null if there is no
+*    second to last node.
+*/
+    secondToLast() {
+        if (this.isEmpty()) {
+            return null;
+        }
+        let runner = this.head;
+        if (!runner.next) {
+            return null;
+        }
+        if (!runner.next.next) {
+            return null;
+        }
+        while (runner.next.next) {
+            runner = runner.next;
+        }
+        return runner.data;
+    }
+
+    /**
+     * Removes the node that has the matching given val as it's data.
+     * - Time: O(?).
+     * - Space: O(?).
+     * @param {any} val The value to compare to the node's data to find the
+     *    node to be removed.
+     * @returns {boolean} Indicates if a node was removed or not.
+     */
+    removeVal(val) {
+        if (this.isEmpty()) {
+            return false;
+        }
+        let runner = this.head;
+        if (runner.data === val) {
+            this.removeHead()
+            return true;
+        }
+        while (runner.next) {
+            if (runner.next.data === val) {
+                if (runner.next.next) {
+                    runner.next = runner.next.next
+                    return true;
+                }
+                runner.next = null;
+                return true;
+            }
+            runner = runner.next;
+        }
+        return false;
+    }
+
+    /**
+     * Concatenates the nodes of a given list onto the back of this list.
+     * - Time: O(?).
+     * - Space: O(?).
+     * @param {SinglyLinkedList} addList An instance of a different list whose
+     *    whose nodes will be added to the back of this list.
+     * @returns {SinglyLinkedList} This list with the added nodes.
+     */
+    concat(addList) { 
+        if (this.isEmpty()) {
+            this.head = addList.head;
+            return this;
+        }
+        let runner = this.head;
+        while (runner.next) {
+            runner = runner.next;
+        }
+        runner.next = addList.head;
+        return this;
+    }
+
+    /**
+     * Finds the node with the smallest data and moves that node to the front of
+     * this list.
+     * - Time: O(?).
+     * - Space: O(?).
+     * @returns {SinglyLinkedList} This list.
+     */
+    moveMinToFront() {
+        if (this.isEmpty()) {
+            return this;
+        }
+        let runner = this.head;
+        let smallest = this.head;
+        let smallestPast = this.head;
+        while (runner.next) {
+            if(runner.next.data < smallest.data){
+                smallest = runner.next
+                smallestPast = runner;
+            }
+            runner = runner.next;
+        }
+        if(smallest.next){
+            smallestPast.next = smallest.next
+        }
+        smallest.next = this.head;
+        this.head = smallest;
+        return this;
+    }
+}
+
+/******************************************************************* 
+Multiple test lists already constructed to test your methods on.
+Below commented code depends on insertAtBack method to be completed,
+after completing it, uncomment the code.
+*/
+const emptyList = new SinglyLinkedList();
+
+const singleNodeList = new SinglyLinkedList().insertAtBackMany([1]);
+const biNodeList = new SinglyLinkedList().insertAtBackMany([1, 2]);
+const firstThreeList = new SinglyLinkedList().insertAtBackMany([1, 2, 3]);
+const secondThreeList = new SinglyLinkedList().insertAtBackMany([4, 5, 6]);
+const unorderedList = new SinglyLinkedList().insertAtBackMany([
+    -5, -10, 4, -3, 6, 1, -7, -2,
+]);
+
+/* node 4 connects to node 1, back to head */
+const perfectLoopList = new SinglyLinkedList().insertAtBackMany([1, 2, 3, 4]);
+perfectLoopList.head.next.next.next = perfectLoopList.head;
+
+/* node 4 connects to node 2 */
+const loopList = new SinglyLinkedList().insertAtBackMany([1, 2, 3, 4]);
+loopList.head.next.next.next = loopList.head.next;
+
+const sortedDupeList = new SinglyLinkedList().insertAtBackMany([
+    1, 1, 1, 2, 3, 3, 4, 5, 5,
+]);
+
+//Print your list like so:
+//  console.log(firstThreeList.toArr());
+
+
+let mySll = new SinglyLinkedList()
+let secondSll = new SinglyLinkedList()
+
+mySll.insertAtBackMany([6, 2, 3, 4])
+secondSll.insertAtBackMany([5, 6, 7, 8])
