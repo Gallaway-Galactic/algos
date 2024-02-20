22d61e3ab9f88ac88555b9606c35da080dc08911
Joshua <gallaway.joshua@gmail.com>
2024-02-20 10:40:03 -0700

    added methods to queue, also added Linked List queue methods as well

--- Queues/queue.js
+++ Queues/queue.js
@@ -5,9 +5,9 @@
  */
 class Queue {
     constructor() {
-      this.items = [];
+        this.items = [];
     }
-  
+
     /**
      * Adds a new given item to the back of this queue.
      * - Time: O(1) constant.
@@ -15,8 +15,11 @@ class Queue {
      * @param {any} item The new item to add to the back.
      * @returns {number} The new size of this queue.
      */
-    enqueue(item) {}
-  
+    enqueue(item) {
+        this.items.push(item);
+        return this.size();
+    }
+
     /**
      * Removes and returns the first item of this queue.
      * - Time: O(n) linear, due to having to shift all the remaining items to
@@ -24,31 +27,97 @@ class Queue {
      * - Space: O(1) constant.
      * @returns {any} The first item or undefined if empty.
      */
-    dequeue() {}
-  
+    dequeue() {
+        return this.items.shift();
+    }
+
     /**
      * Retrieves the first item without removing it.
      * - Time: O(1) constant.
      * - Space: O(1) constant.
      * @returns {any} The first item or undefined if empty.
      */
-    front() {}
-  
+    front() {
+        return this.items[0];
+    }
+
     /**
      * Returns whether or not this queue is empty.
      * - Time: O(1) constant.
      * - Space: O(1) constant.
      * @returns {boolean}
      */
-    isEmpty() {}
-  
+    isEmpty() {
+        return this.size() > 0;
+        return this.size() > 0 ? true : false;
+    }
+
     /**
      * Retrieves the size of this queue.
      * - Time: O(1) constant.
      * - Space: O(1) constant.
      * @returns {number} The length.
      */
-    size() {}
-  }
-  
-  /* Rebuild the above class using a linked list instead of an array. */
+    size() {
+        return this.items.length
+    }
+}
+
+/* Rebuild the above class using a linked list instead of an array. */
+
+const myQ = new Queue();
+
+class Qnode {
+
+    constructor(data) {
+        this.data = data
+        this.next = null
+    }
+}
+
+class LLQ {
+
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+    }
+
+    isEmpty() {
+        return this.head === null;
+    }
+
+    enqueue(value) {
+        const newNode = new Qnode(value);
+        if (!this.head) {
+            this.head = newNode
+            this.tail = newNode
+            this.size++;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+            this.size++;
+        }
+        return this.size;
+    }
+
+    dequeue() {
+
+        if (this.isEmpty()) {
+            return null;
+        }
+
+        if (this.size === 1) {
+            this.tail = null;
+        }
+        let oldHead = this.head;
+        this.head = this.head.next;
+        this.size--;
+        return oldHead.data
+    }
+
+}
+
+const myQll = new LLQ()
+
+console.log(myQll.enqueue(1))
